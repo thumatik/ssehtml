ebf93a06008f727eabeab03fe1696e3c8b502472
thumatik <71712466+thumatik@users.noreply.github.com>
2025-07-22 00:24:43 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
